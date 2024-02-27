77a51adae76cdecb5d85db5729d5c59fa76d1513
madhukar <madhukar@gmail.com>
2024-02-27 19:21:04 +0530

    initial commit done

---
+++ src/main/java/com/springbootweb/HomeController.java
@@ -0,0 +1,21 @@
+package com.springbootweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+	
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "home";
+	}
+	
+	@RequestMapping("/contact")
+	public String contact()
+	{
+		return "contact";
+	}
+
+}
---
+++ src/main/webapp/views/home.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<h1 align="center"> this is first home please click on below link to view contact page </h1>
+<a href="contact">Click Here</a>
+</body>
+</html>
